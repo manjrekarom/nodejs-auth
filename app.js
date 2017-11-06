c5b0508f897f2c0b28e1d8310e93a3661e04f784
Omkar Manjrekar <manjrekarom@gmail.com>
2017-11-06 15:27:47 +0530

    Added authentication

--- app.js
+++ app.js
@@ -1,29 +1,118 @@
-const express = require('express'),
-      app = express(),
-      bodyParser = require('body-parser'),
-      session = require('express-session'),
-      mongoose = require('mongoose'),
-      path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const User = require('./models/user.js');
+const path = require('path');
+const MongoStore = require('connect-mongo')(session);
+const hbs = require('hbs');
+
+mongoose.connect('mongodb://localhost:27017/nodejsauth');
+let db = mongoose.connection;
+mongoose.Promise = global.Promise;
+
+app.set('view enigne', 'hbs');
 
 app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {domain: 'localhost'},
+    store: new MongoStore({
+        mongooseConnection: db
+    })
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'public','login.html'));
+    console.log(req.session);
+    if(req.session.userId){
+        res.redirect('/home');
+    }
+    else 
+        res.sendFile(path.join(__dirname,'public','login.html'));
+});
+
+app.get('/home', (req, res) => {
+    if(req.session.userId){
+        User.findOne({
+            _id: req.session.userId
+        }).then((user) => {
+            res.render('home.hbs', {
+                username: user.username
+            });
+        });
+    }
+    else {
+        res.status(401).send('Not Authenticated!');
+    }
 });
 
 app.post('/registerrequest', (req, res) => {
+    console.log('Inside registerrequest');
+    
     let username = req.body.username;
     let password = req.body.password;
     let email = req.body.email;
+    if(username && password && email) {
+        let userData = {
+            username,
+            password,
+            email
+        };
+        let user = new User(userData);
+        user.save().then((doc) => {
+            if(!doc)
+                res.status(400).send('Bad request');
+            else res.send(_.pick(doc, ['_id','username','email']));
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+    } else {
+        res.status(400).send('Bad request');
+    }
 });
 
 app.post('/loginrequest', (req, res) => {
+    
+    console.log('Inside loginrequest');    
     let username = req.body.username;
     let password = req.body.password;
+    let userData = {username, password};
+    
+    User.authenticate(username, password, (err, user) => {
+        // console.log(user);
+        if(!user) {
+            res.status(401).send('Not authenticated');
+        }
+        else {
+            // res.redirect('/home.html');
+            req.session.userId = user._id;
+            res.redirect('/home');
+        }
+    });
+    
+});
+
+app.get('/logout', (req, res) => {
+    if(req.session) {
+        req.session.destroy((err) => {
+            if(err) {
+                return next(err);
+            } else {
+                return res.redirect('/');
+            }
+        });
+    }
 });
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000!');
+    console.log(`Server running on port ${3000}!`);
 });
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 let UserSchema = new mongoose.Schema({
     email: {
@@ -16,11 +17,39 @@ let UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    },
-    passwordConf: {
-        type: String,
-        required: true,
     }
 });
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+    bcrypt.hash(user.password, 10, function (err, hash){
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    });
+});
+
+UserSchema.statics.authenticate = function (username, password, callback) {
+    User.findOne({ username: username })
+    .exec(function (err, user) {
+        if (err) {
+            return callback(err)
+        } else if (!user) {
+            var err = new Error('User not found.');
+            err.status = 401;
+            return callback(err);
+        }
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (result === true) {
+                return callback(null, user);
+            } else {
+                return callback(true);
+            }
+        })
+    });
+}
+
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
